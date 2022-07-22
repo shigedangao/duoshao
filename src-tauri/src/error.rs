cf7f0907fa6680e7fbc60adacdd71dc84cc37422
shigedangao <marc.inthaamnouay@gmail.com>
2022-07-22 23:10:36 +0200

    load dictionary asynchronously. bump tauri to latest version

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,11 +1,13 @@
 use tauri::InvokeError;
+use tokio::task::JoinError;
 
 #[derive(Debug)]
 pub enum Error {
     Lock(String),
     NoDefinitions,
     Export(String),
-    IO(String)
+    IO(String),
+    Load(String)
 }
 
 impl std::fmt::Display for Error {
@@ -14,7 +16,8 @@ impl std::fmt::Display for Error {
             Error::Lock(target) => write!(f, "Unable to acquired the lock for: {target}"),
             Error::NoDefinitions => write!(f, "No definition has been founded for the targeted language"),
             Error::Export(msg) => write!(f, "Unable generate export for reasons: {msg}"),
-            Error::IO(msg) => write!(f, "Unable to write the targeted file for reasons: {msg}")
+            Error::IO(msg) => write!(f, "Unable to write the targeted file for reasons: {msg}"),
+            Error::Load(msg) => write!(f, "Unable to load dictionary {msg}")
         }
     }
 }
@@ -24,5 +27,11 @@ impl std::error::Error for Error {}
 impl From<Error> for InvokeError {
     fn from(err: Error) -> Self {
         InvokeError::from_anyhow(anyhow::Error::new(err))
+    }
+}
+
+impl From<JoinError> for Error {
+    fn from(err: JoinError) -> Self {
+        Error::Load(err.to_string())
     }
 }
